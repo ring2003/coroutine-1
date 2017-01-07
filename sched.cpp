88eb9dba3b3006001198286faa0f0ed7c676e3ee
louxiao.lx <louxiao.lx@alibaba-inc.com>
2017-01-07 22:54:19 +0800

    修正一个th改成tid但是switch_debug宏内部代码被遗漏的bug

--- sched.cpp
+++ sched.cpp
@@ -11,13 +11,13 @@
 int coro_yield_uthread(uthread_t th, int yield_value)
 {
 #ifdef SWITCH_DEBUG
-    printf("yield begin current id: %d, target: %d\n", ctx.cur, th->tid);
+    printf("yield begin current id: %d, target: %d\n", ctx.cur, th);
 #endif
     int old_tid = ctx.cur;
     int ret = coro_yield(ctx.ths[th]->coro, yield_value);
     ctx.cur = old_tid;
 #ifdef SWITCH_DEBUG
-    printf("yield end current: %d from: %d\n", ctx.cur, th->tid);
+    printf("yield end current: %d from: %d\n", ctx.cur, th);
 #endif
     return ret;
 
@@ -26,13 +26,13 @@ int coro_yield_uthread(uthread_t th, int yield_value)
 int coro_resume_value_uthread(uthread_t th, int yield_value)
 {
 #ifdef SWITCH_DEBUG
-    printf("resume begin: current id: %d target: %d\n", ctx.cur, th->tid);
+    printf("resume begin: current id: %d target: %d\n", ctx.cur, th);
 #endif
     int old_tid = ctx.cur;
     int ret = coro_resume_value(ctx.ths[th]->coro, yield_value);
     ctx.cur = old_tid;
 #ifdef SWITCH_DEBUG
-    printf("resume end: current: %d from id: %d\n", ctx.cur, th->tid);
+    printf("resume end: current: %d from id: %d\n", ctx.cur, th);
 #endif
     return ret;
 }
@@ -41,11 +41,11 @@ int coro_resume_uthread(uthread_t th)
 {
     int old_tid = ctx.cur;
 #ifdef SWITCH_DEBUG
-    printf("resume begin: current id: %d target: %d\n", ctx.cur, th->tid);
+    printf("resume begin: current id: %d target: %d\n", ctx.cur, th);
 #endif
     int ret = coro_resume(ctx.ths[th]->coro);
 #ifdef SWITCH_DEBUG
-    printf("resume end: back from id: %d\n", th->tid);
+    printf("resume end: back from id: %d\n", th);
 #endif
     ctx.cur = old_tid;
     return ret;
